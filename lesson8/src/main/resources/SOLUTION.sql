3796672d520582b41b5c39a9bf96e740054bf7e6
ulugbek <ulugbekyusupov272@gmail.com>
2024-02-13 03:08:11 +0500

    finish lesson8

---
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -0,0 +1,13 @@
+SELECT MIN(birthday)
+FROM student;
+
+SELECT MIN(payment_date)
+FROM payment;
+
+SELECT AVG(mark)
+FROM mark
+WHERE subject_id = (SELECT id FROM subject WHERE name = 'Math');
+
+SELECT MIN(amount)
+FROM payment
+WHERE type_id = (SELECT id FROM paymenttype WHERE name = 'WEEKLY');
