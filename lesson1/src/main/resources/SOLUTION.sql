36cc4bd488b519e4615825e4f4816b5b1b42c840
ulugbek <ulugbekyusupov272@gmail.com>
2024-02-13 00:46:07 +0500

    change some

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,39 +1,36 @@
 USE Univesity;
 
 CREATE TABLE Student IN University{
-    id bigint,
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255),
     birthday date,
-    group int,
-    PRIMARY KEY (id)
+    group int
 }
 
 CREATE TABLE Subject IN University{
-    id bigint,
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name varchar(255),
     description varchar(255),
-    grade int,
-    PRIMARY KEY (id)
+    grade int
     }
 
 CREATE TABLE PaymentType IN University{
-    id bigint,
-    name varchar(255),
-    PRIMARY KEY (id)
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name varchar(255)
     }
 
 CREATE TABLE Payment IN University{
-    id bigint,
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
     type_id bigint,
     amount decimal,
     student_id bigint,
     payment_date date,
-    foreign key(type_id) REFERENCES PaymentType(id),
-    foreign key(student_id) REFERENCES Student(id)
+    foreign key (type_id) REFERENCES PaymentType(id),
+    foreign key (student_id) REFERENCES Student(id)
     }
 
 CREATE TABLE Mark IN University{
-    id bigint,
+    id bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
     student_id bigint,
     subject_id bigint,
     mark int,
