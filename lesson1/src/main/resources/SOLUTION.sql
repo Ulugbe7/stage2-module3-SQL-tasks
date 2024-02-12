7697ef085084dca7e76c567f12f88a89ac13fc2e
ulugbek <ulugbekyusupov272@gmail.com>
2024-02-13 00:41:50 +0500

    finish lesson1

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -1,2 +1,42 @@
+USE Univesity;
 
+CREATE TABLE Student IN University{
+    id bigint,
+    name varchar(255),
+    birthday date,
+    group int,
+    PRIMARY KEY (id)
+}
 
+CREATE TABLE Subject IN University{
+    id bigint,
+    name varchar(255),
+    description varchar(255),
+    grade int,
+    PRIMARY KEY (id)
+    }
+
+CREATE TABLE PaymentType IN University{
+    id bigint,
+    name varchar(255),
+    PRIMARY KEY (id)
+    }
+
+CREATE TABLE Payment IN University{
+    id bigint,
+    type_id bigint,
+    amount decimal,
+    student_id bigint,
+    payment_date date,
+    foreign key(type_id) REFERENCES PaymentType(id),
+    foreign key(student_id) REFERENCES Student(id)
+    }
+
+CREATE TABLE Mark IN University{
+    id bigint,
+    student_id bigint,
+    subject_id bigint,
+    mark int,
+    foreign key(student_id) REFERENCES Student(id),
+    foreign key(subject_id) REFERENCES Subject(id)
+    }
