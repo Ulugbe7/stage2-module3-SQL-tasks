14efa398a6c8d1ec698231f283f7c2a435d30114
ulugbek <ulugbekyusupov272@gmail.com>
2024-02-13 03:24:57 +0500

    fix lesson10

--- lesson10/src/main/resources/SOLUTION.sql
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -1,8 +1,8 @@
 SELECT *
-FROM subject
-WHERE id = (SELECT subject_id from mark GROUP BY subject_id ORDER BY AVG(mark) DESC LIMIT 1);
+FROM subject s
+WHERE id = (SELECT subject_id from mark B GROUP BY subject_id ORDER BY AVG(mark) DESC LIMIT 1);
 
-SELECT *
-FROM student
-         JOIN payment p ON id = student_id
-WHERE amount < (SELECT AVG(amount) AS avg_payment_amount FROM payment);
+SELECT s.*
+FROM student s
+         JOIN payment p ON s.id = p.student_id
+WHERE p.amount < (SELECT AVG(amount) AS avg_payment_amount FROM payment);
